75defd998a14903347f5b08c310237371ec78e49
beniamin-benek <46487463+beniamin-benek@users.noreply.github.com>
2019-02-21 02:39:05 +0100

    Formatowanie

--- src/NumbersCounter.java
+++ src/NumbersCounter.java
@@ -6,24 +6,24 @@ class NumbersCounter {
 	static int[] countNumbers(List<Integer> array) {
 		if(array == null || array.size() == 0) throw new NullPointerException();
 		
-        int[] counter = new int[getTheBiggest(array) + 1];
+		int[] counter = new int[getTheBiggest(array) + 1];
 
-        for (int i = 0; i < array.size(); i++)
-            counter[array.get(i)]++; //każde pole tablicy mówi o liczbie wystąpień danej liczby z listy
- 
-        return counter;
+		for (int i = 0; i < array.size(); i++)
+		    counter[array.get(i)]++; //każde pole tablicy mówi o liczbie wystąpień danej liczby z listy
+
+		return counter;
 	}
 	
 	static void printInfo(List<Integer> array) {
 		int[] counter = countNumbers(array);
 		
 		System.out.println("Ciąg wczytany z pliku: " + array);
-        System.out.println("Unikalne wartości: " + createTreeSetFromArray(array)); //dodatkowa informacja
+        	System.out.println("Unikalne wartości: " + createTreeSetFromArray(array)); //dodatkowa informacja
         
-        for (int i = 0; i < counter.length; i++) {
-        	if(counter[i] != 0)
-        		System.out.println(i + " - liczba wystąpień: " + counter[i]);
-        }
+		for (int i = 0; i < counter.length; i++) {
+			if(counter[i] != 0)
+				System.out.println(i + " - liczba wystąpień: " + counter[i]);
+		}
 	}
 
 	private static int getTheBiggest(List<Integer> array) {
@@ -36,8 +36,8 @@ class NumbersCounter {
 	}
 	
 	private static TreeSet<Integer> createTreeSetFromArray(List<Integer> array) {
-        TreeSet<Integer> uniqeArray = new TreeSet<>(array); //kopiuje wartości z podanej listy do nowego zbioru, co niweluje powtórzenia
-        return uniqeArray;
+		TreeSet<Integer> uniqeArray = new TreeSet<>(array); //kopiuje wartości z podanej listy do nowego zbioru, co niweluje powtórzenia
+		return uniqeArray;
 	}
 	
 }
