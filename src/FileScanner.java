2261a546c6694e90bdd377f1dd7fda491f39f4a7
beniamin-benek <46487463+beniamin-benek@users.noreply.github.com>
2019-02-21 02:43:03 +0100

    Formatowanie

--- src/FileScanner.java
+++ src/FileScanner.java
@@ -7,23 +7,23 @@ import java.util.LinkedList;
 public class FileScanner {
 	
 	static File openExternalFile(String patchname) throws FileNotFoundException {
-	if(patchname == null) throw new FileNotFoundException();
-        File file = new File(patchname);
-        return file;
+		if(patchname == null) throw new FileNotFoundException();
+			File file = new File(patchname);
+		return file;
 	}
 	
 	static List<Integer> createArrayFromFile (File file) throws FileNotFoundException {
-        Scanner scanner = new Scanner(file);
-        List<Integer> array = new LinkedList<>();
-        int currentNumber;
-        
-        while (scanner.hasNextLine()) {
-        	currentNumber = scanner.nextInt();
-        	array.add(currentNumber);
-        }
-        scanner.close();
-        
-        return array;
+		Scanner scanner = new Scanner(file);
+		List<Integer> array = new LinkedList<>();
+		int currentNumber;
+
+		while (scanner.hasNextLine()) {
+			currentNumber = scanner.nextInt();
+			array.add(currentNumber);
+		}
+		scanner.close();
+
+		return array;
 	}
 	
 }
